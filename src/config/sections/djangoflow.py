181ac291b40f9dc808a1bc92b3e4c2b7128b658c
Yevhen Bondar <eugenij.bondar@gmail.com>
2022-08-18 10:27:51 +0300

    add Dataflow notifications

--- src/config/sections/djangoflow.py
+++ src/config/sections/djangoflow.py
@@ -1,5 +1,4 @@
 from df_auth.djangoflow.settings import *
+from df_notifications.djangoflow.settings import *
 
-DF_APPS = [
-    *DF_AUTH_APPS,
-]
+DF_APPS = list(set(DF_AUTH_APPS) | set(DF_NOTIFICATIONS_APPS))
